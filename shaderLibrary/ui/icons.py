0321cd21a38a62fe4f559daed6fe7b4c1435d7a1
MaxRocamora <maxirocamora@gmail.com>
2019-05-25 17:38:17 -0300

    Added Icons

---
+++ shaderLibrary/ui/icons.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# --------------------------------------------------------------------------------------------
+# Icon Qt Dict for interfaces
+# --------------------------------------------------------------------------------------------
+
+# IMPORTS
+import os
+from PySide2 import QtGui
+iconPath = os.path.join(os.path.dirname(__file__), 'icons')
+print iconPath
+IconLib = {}
+
+for icon in os.listdir(iconPath):
+    iconName, ext = os.path.splitext(icon)
+    IconLib[iconName.lower()] = QtGui.QIcon(os.path.join(iconPath, icon))
+
+
+def getIcon(name):
+    ''' returns icon from iconlib '''
+    return IconLib[name.lower()]
