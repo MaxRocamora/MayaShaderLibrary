f65d426637da6a014e3ffaf34080707c4fee8487
MaxRocamora <maxirocamora@gmail.com>
2019-05-25 21:05:16 -0300

    added maya plugin and menu

---
+++ plugin/maya/Arcane_ShaderLibrary.py
@@ -0,0 +1,42 @@
+﻿# -*- coding: utf-8 -*-
+###########################################################################
+#
+# Maya Plugin
+# Author: Maximiliano Rocamora - maxirocamora@gmail.com
+# Release: 05/2019
+#
+###########################################################################
+
+import sys
+import os
+import maya.OpenMayaMPx as OpenMayaMPx
+
+
+class EnvironmentVariableNotFound(Exception):
+    '''Raise when a required environment variable os not found. '''
+    pass
+
+
+# Main PATHS
+try:
+    ARCANE_SHADERLIB_PATH = os.environ['ARCANE_SHADERLIB_PATH']
+    sys.path.append(ARCANE_SHADERLIB_PATH)
+except KeyError:
+    raise EnvironmentVariableNotFound(
+        'ARCANE_SHADERLIB_PATH Env Var not found.')
+
+kPluginCmdName = "ARCANE Tools ShaderLibrary 1.0"
+
+
+def initializePlugin(mobject):
+    ''' Initialize the script plug-in '''
+    mplugin = OpenMayaMPx.MFnPlugin(mobject)
+    from plugin.ui.menu import menuCreator
+    menuCreator()
+
+
+def uninitializePlugin(mobject):
+    ''' Uninitialize the script plug-in '''
+    mplugin = OpenMayaMPx.MFnPlugin(mobject)
+    from plugin.ui.menu import menuCreator
+    menuCreator().unloadMenu()
