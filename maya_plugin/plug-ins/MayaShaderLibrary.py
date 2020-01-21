fb86b4dfc2cf5b8c3ccc934c495471034df0ef1f
MaxRocamora <maxirocamora@gmail.com>
2020-01-21 01:20:49 -0300

    maya plugin, renamed, added env check, removed menus

--- maya_plugin/plug-ins/MayaShaderLibrary.py
+++ maya_plugin/plug-ins/MayaShaderLibrary.py
@@ -3,35 +3,26 @@
 #
 # Maya Plugin 2018
 # Author: Maximiliano Rocamora - maxirocamora@gmail.com
-# Release: 05/2019
+# First Release: 05/2019
 #
 ###########################################################################
 
-import sys
-import os
 import maya.OpenMayaMPx as OpenMayaMPx
+from msl import VALID_ENV
 
-# Add ASL package root
-try:
-    arcanePath = os.environ['ARCANE_SHADER_LIBRARY']
-except KeyError:
-    print 'Missing Maya.env entry: ARCANE_SHADER_LIBRARY'
 
-if arcanePath not in sys.path:
-    sys.path.append(arcanePath)
-
-kPluginCmdName = "ARCANE Tools ShaderLibrary 1.0"
+kPluginCmdName = "Maya Shader Library 1.1"
 
 
 def initializePlugin(mobject):
     ''' Initialize the script plug-in '''
+    if not VALID_ENV:
+        print 'Unable to initializePlugin', kPluginCmdName
+        return
+
     mplugin = OpenMayaMPx.MFnPlugin(mobject)
-    from msl.ui.menu.menu import menuCreator
-    menuCreator()
 
 
 def uninitializePlugin(mobject):
     ''' Uninitialize the script plug-in '''
     mplugin = OpenMayaMPx.MFnPlugin(mobject)
-    from msl.ui.menu.menu import menuCreator
-    menuCreator().unloadMenu()
