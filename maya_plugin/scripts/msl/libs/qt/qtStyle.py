fe4bb3e2ae8f3a11739e1b62ac7378af01d98964
MaxRocamora <maxirocamora@gmail.com>
2020-01-21 01:22:05 -0300

    renamed global filepath from init

--- maya_plugin/scripts/msl/libs/qt/qtStyle.py
+++ maya_plugin/scripts/msl/libs/qt/qtStyle.py
@@ -3,10 +3,8 @@
 '''
 # Style QT Methods for Arcane Interfaces
 '''
-import os
 from PySide2 import QtCore, QtGui
-appPath = os.path.dirname(__file__)
-from ... import sshFile, arcaneIcon
+from ... import QSS_FILE, ICON_FILE
 
 
 def cssMainWindow(appPath, mainWidget, objName, windowTitle):
@@ -19,9 +17,9 @@ def cssMainWindow(appPath, mainWidget, objName, windowTitle):
             objName (string) Name of the qt object for window detection/destroy.
             windowTitle (string) Set the name of the main window
     '''
-    mainWidget.setWindowIcon(QtGui.QIcon(arcaneIcon))
+    mainWidget.setWindowIcon(QtGui.QIcon(ICON_FILE))
     mainWidget.setObjectName(objName)
     mainWidget.setWindowTitle(windowTitle)
     mainWidget.setAttribute(QtCore.Qt.WA_DeleteOnClose, True)
-    with open(sshFile, "r") as fh:
+    with open(QSS_FILE, "r") as fh:
         mainWidget.setStyleSheet(fh.read())
