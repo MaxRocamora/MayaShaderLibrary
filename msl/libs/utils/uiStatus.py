caacc93ef74a7031066774494ebc16d8e8dbfcf3
Maximilliano Rocamora <maxirocamora@gmail.com>
2021-11-19 01:40:58 -0300

    python 3 support, pep, refactor

--- msl/libs/utils/uiStatus.py
+++ msl/libs/utils/uiStatus.py
@@ -8,15 +8,16 @@
 
 
 class Statusbar():
-    def __init__(self, _qwidget):
+    def __init__(self, qBarWidget):
         '''
-         _qwidget ::: widget of the status bar ui
+        Args:
+            qBarWidget (widget) widget of the status bar ui
         '''
-        self.uiBar = _qwidget
+        self.uiBar = qBarWidget
         self.success('', timed=0)
 
     def success(self, msg, timed=0):
-        # Color green message / For successful operations
+        '''Color green message / for successful operations'''
         if timed > 0:
             self.uiBar.showMessage(msg, timed)
         else:
@@ -25,22 +26,22 @@ class Statusbar():
             "color: lime; background: rgba(20, 20, 20, 255);")  # green warning
 
     def inform(self, msg, timed=0):
-        # Color orange message / for inform messages
+        '''Color orange message / for inform messages'''
         if timed > 0:
             self.uiBar.showMessage(msg, timed)
         else:
             self.uiBar.showMessage(msg)
         self.uiBar.setStyleSheet(
-            "color: orange; background: rgba(20, 20, 20, 255);")  # orange warning
+            "color: orange; background: rgba(20, 20, 20, 255);")
 
     def warning(self, msg):
-        # Color yellow message / for warning messages
+        '''Color yellow message / for warning messages'''
         self.uiBar.showMessage(msg)
         self.uiBar.setStyleSheet(
-            "color: yellow; background: rgba(20, 20, 20, 255);")  # yellow warning
+            "color: yellow; background: rgba(20, 20, 20, 255);")
 
     def error(self, msg):
-        # Color RED message / # for failed operations
+        '''Color RED message / for failed operations'''
         self.uiBar.showMessage(msg)
         self.uiBar.setStyleSheet(
-            "color: red; background: rgba(20, 20, 20, 255);")  # red warning
+            "color: red; background: rgba(20, 20, 20, 255);")
