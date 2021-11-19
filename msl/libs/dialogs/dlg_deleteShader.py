a423f0c859ac84fd72192aabfd01db0ffde6ecef
Maximilliano Rocamora <maxirocamora@gmail.com>
2021-11-19 11:47:54 -0300

    docstring, renamed ui class

--- msl/libs/dialogs/dlg_deleteShader.py
+++ msl/libs/dialogs/dlg_deleteShader.py
@@ -1,17 +1,19 @@
-# -*- coding: utf-8 -*-
 # --------------------------------------------------------------------------------------------
-# ARCANE Shader Library deleteShader dialog
+# Maya Shader Library
+# Author: maxirocamora@gmail.com
+#
+# Shader Library deleteShader dialog
 # This class ask for user confirmation on shader physical disk deletion
 # --------------------------------------------------------------------------------------------
-
 from PySide2 import QtWidgets
+
+msgStr = {
+    'title': 'Confirm Deletion',
+    'deleteConfirmation': 'You are about to delete shader files.'
+}
 
 
 class deleteShaderDialog():
-    msgStr = {
-        'title': 'Confirm Deletion',
-        'deleteConfirmation': 'You are about to delete shader files.'
-    }
 
     def __init__(self, shaderClass, observer):
         '''
@@ -20,19 +22,19 @@ class deleteShaderDialog():
             shaderClass (class) shader class calling this input
             observer (class) observer holding ui
         '''
-        self.ui = observer.ui
+        self.observer = observer
         self.shader = shaderClass
 
     def __call__(self):
         ''' open qt dialog box for rename shader '''
-        msgBox = QtWidgets.QMessageBox(self.ui)
-        msgBox.setWindowTitle(self.msgStr['title'])
+        msgBox = QtWidgets.QMessageBox(self.observer.ui)
+        msgBox.setWindowTitle(msgStr['title'])
         msgBox.setStandardButtons(
             QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Cancel)
         msgBox.setStyleSheet("background: rgba(40, 40, 40, 255);")
         msgBox.setIcon(QtWidgets.QMessageBox.Warning)
-        msgBox.setText(self.msgStr['deleteConfirmation'])
+        msgBox.setText(msgStr['deleteConfirmation'])
         choice = msgBox.exec_()
         if choice == QtWidgets.QMessageBox.Ok:
             self.shader.delete()
-            self.ui.categoryCC.refreshCategoryTab()
+            self.observer.main.categoryCC.refreshCategoryTab()
