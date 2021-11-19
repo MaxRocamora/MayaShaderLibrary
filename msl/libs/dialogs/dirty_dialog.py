8047051383a43ab5af02b0ec005dc5773c2528de
Maximilliano Rocamora <maxirocamora@gmail.com>
2021-11-19 01:38:06 -0300

    Create dirty_dialog.py

---
+++ msl/libs/dialogs/dirty_dialog.py
@@ -0,0 +1,22 @@
+import maya.cmds as cmds
+
+
+def dirty_file_dialog():
+    ''' Ask user to save their current scene
+    if it was modified since last save.
+    '''
+    if not cmds.file(query=True, modified=True):
+        return
+
+    msg = 'Current file is modified. Do you want to save this file?'
+    result = cmds.confirmDialog(title='Save changes',
+                                message=msg,
+                                button=['yes', 'no'],
+                                defaultButton='yes',
+                                cancelButton='no',
+                                dismissString='no')
+    if result == 'yes':
+        try:
+            cmds.file(save=True, type='mayaAscii')
+        except RuntimeError as e:
+            print(str(e))
