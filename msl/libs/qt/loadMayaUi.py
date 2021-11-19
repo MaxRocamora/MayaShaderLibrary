79870a0451c84e6b3cce05acab005473ada109ec
Maximilliano Rocamora <maxirocamora@gmail.com>
2021-11-19 01:40:48 -0300

    python 2.7 and 3.7 get_maya_window()

--- msl/libs/qt/loadMayaUi.py
+++ msl/libs/qt/loadMayaUi.py
@@ -1,40 +1,25 @@
 # -*- coding: utf-8 -*-
 # --------------------------------------------------------------------------------------------
-# QtDesigner UI File loader for Maya Pyside2
+# QtDesigner UI File loader for Maya PySide2
+# Python 2/3
 # --------------------------------------------------------------------------------------------
 
-from cStringIO import StringIO
-import xml.etree.ElementTree as xml
-import maya.OpenMayaUI as om
+import sys
 from PySide2 import QtWidgets
-from PySide2.QtGui import *
-from PySide2.QtCore import *
-from PySide2.QtWidgets import *
-import shiboken2 as shiboken
-import pyside2uic
+from shiboken2 import wrapInstance
+from maya import OpenMayaUI as omui
+
+PY_VERSION = sys.version_info.major
+
+if PY_VERSION == 2:
+    from cStringIO import StringIO
+    import xml.etree.ElementTree as xml
+    import pyside2uic as pysideuic
 
 
-def getMayaWindow():
-    maya_window_util = om.MQtUtil.mainWindow()
-    if maya_window_util is not None:
-        return shiboken.wrapInstance(long(maya_window_util), QtWidgets.QMainWindow)
-
-
-def loadUi(uiFile):
-    ''' Parses an UI file '''
-    parsed = xml.parse(uiFile)
-    widget_class = parsed.find('widget').get('class')
-    form_class = parsed.find('class').text
-
-    with open(uiFile, 'r') as f:
-        o = StringIO()
-        frame = {}
-
-        pyside2uic.compileUi(f, o, indent=0)
-        pyc = compile(o.getvalue(), '<string>', 'exec')
-        exec pyc in frame
-
-        # Fetch the base_class and form class based on their type in the xml from designer
-        form_class = frame['Ui_%s' % form_class]
-        base_class = eval('%s' % widget_class)
-        return form_class, base_class
+def get_maya_main_window():
+    main_window_ptr = omui.MQtUtil.mainWindow()
+    if PY_VERSION >= 3:
+        return wrapInstance(int(main_window_ptr), QtWidgets.QWidget)
+    else:
+        return wrapInstance(long(main_window_ptr), QtWidgets.QWidget)
