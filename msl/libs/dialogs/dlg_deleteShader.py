2018f5f0196ca4d0e29efadfd652159723beaf27
Maximilliano Rocamora <maxirocamora@gmail.com>
2021-11-19 12:36:04 -0300

    message vars

--- msl/libs/dialogs/dlg_deleteShader.py
+++ msl/libs/dialogs/dlg_deleteShader.py
@@ -7,10 +7,8 @@
 # --------------------------------------------------------------------------------------------
 from PySide2 import QtWidgets
 
-msgStr = {
-    'title': 'Confirm Deletion',
-    'deleteConfirmation': 'You are about to delete shader files.'
-}
+dlg_title = 'Confirm Deletion'
+dlg_msg = 'You are about to delete selected shader files.'
 
 
 class deleteShaderDialog():
@@ -28,12 +26,12 @@ class deleteShaderDialog():
     def __call__(self):
         ''' open qt dialog box for rename shader '''
         msgBox = QtWidgets.QMessageBox(self.observer.ui)
-        msgBox.setWindowTitle(msgStr['title'])
+        msgBox.setWindowTitle(dlg_title)
         msgBox.setStandardButtons(
             QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Cancel)
         msgBox.setStyleSheet("background: rgba(40, 40, 40, 255);")
         msgBox.setIcon(QtWidgets.QMessageBox.Warning)
-        msgBox.setText(msgStr['deleteConfirmation'])
+        msgBox.setText(dlg_msg)
         choice = msgBox.exec_()
         if choice == QtWidgets.QMessageBox.Ok:
             self.shader.delete()
