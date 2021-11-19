935f827823266aff965fb22da5dde888e3d0946a
Maximilliano Rocamora <maxirocamora@gmail.com>
2021-11-19 14:40:13 -0300

    Update version.py

--- msl/version.py
+++ msl/version.py
@@ -1,21 +1,24 @@
-# -*- coding: utf-8 -*-
+# --------------------------------------------------------------------------------------------
+# Maya Shader Library
+# Author: maxirocamora@gmail.com
+#
+# --------------------------------------------------------------------------------------------
 '''
-
 01/2019 - Start Development
 03/2019 - Test Release
 04/2019 - 1.0.1 / Added version
 02/2020 - fixed import package
+11/2021 - 1.1.0 / python 3, some refactor, typos, pep
+11/2021 - 1.1.1 / docstrings, comments, some fixes
+11/2021 - 1.1.2 / autopin new category, more refactor
 
 '''
 
 VERSION_MAJOR = 1
-VERSION_MINOR = 0
+VERSION_MINOR = 1
 VERSION_PATCH = 2
 
 version = '{}.{}.{}'.format(VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
-__version__ = version
 
-__app__ = 'Maya Shader Library'
-qtWinName = 'Arcane2:Qt_' + __app__ + '_ui'
-
-__all__ = ['version', '__version__', '__app__', 'qtWinName']
+app_name = 'Maya Shader Library'
+qtWinName = 'Arcane2:Qt_' + app_name + '_ui'
