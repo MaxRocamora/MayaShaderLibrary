c1811615b6dbdd1c98fd3f4752f287c46cf33408
Maximilliano Rocamora <maxirocamora@gmail.com>
2022-05-04 16:54:36 -0300

    Create logger.py

---
+++ msl/libs/logger.py
@@ -0,0 +1,21 @@
+# --------------------------------------------------------------------------------------------
+# Stream Logger
+# --------------------------------------------------------------------------------------------
+import logging
+
+log = logging.getLogger('ShaderLibrary')
+stream_handler = logging.StreamHandler()
+formatter = logging.Formatter('ShaderLibrary - %(levelname)-7s | %(message)s')
+stream_handler.setFormatter(formatter)
+log.propagate = False
+log.setLevel(logging.DEBUG)
+if len(log.handlers) == 0:
+    log.addHandler(stream_handler)
+
+
+if __name__ == '__main__':
+    log.info('test info')
+    log.warning('test warning')
+    log.error('test error')
+    log.critical('test critical')
+    log.debug('test debug')
