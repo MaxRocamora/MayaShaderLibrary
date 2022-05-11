2b011fc4c53b3d9d3f2cf8e06c493a7cfe56b8d7
Maximilliano Rocamora <maxirocamora@gmail.com>
2022-05-11 20:26:21 -0300

    refactor, log, snake_case

--- msl/libs/category.py
+++ msl/libs/category.py
@@ -1,6 +1,7 @@
 # ----------------------------------------------------------------------------------------
 # Maya Shader Library
 # Author: maxirocamora@gmail.com
+# https://github.com/MaxRocamora/MayaShaderLibrary
 #
 # Category Class
 #
@@ -8,60 +9,50 @@
 import os
 import subprocess
 
-from msl import LIBRARY_SHADERS_PATH
-from msl.libs.dialogs.dlg_inform import informationDialog
+from msl.config import LIBRARY_SHADERS_PATH
+from msl.libs.dialogs.dlg_inform import information_dialog
 from msl.libs.shader import Shader as Shader
+from msl.libs.logger import log
 
 
 class Category():
     def __init__(self, category, path):
         ''' When a category is created, stores all its shaders '''
         self._name = category
-        self.baseFolder = path
-        self._shaders = self.collectShaders()
+        self._path = path
+        self._shaders = self.collect_shaders()
 
     def __str__(self):
-        return f"Category Class {self.name}"
+        return f"Category: {self.name()}"
 
-    # ------------------------------------------------------------------------------------
-    # Properties
-    # ------------------------------------------------------------------------------------
-
-    @property
     def name(self):
         return self._name
 
-    @property
     def folder(self):
         ''' physical path of ths shader '''
-        return os.path.abspath(os.path.join(self.baseFolder, self.name))
+        return os.path.abspath(os.path.join(self._path, self.name()))
 
-    def shaders(self, reload=False):
-        '''
-        Return list of shaders of this category
+    def shaders(self, _reload=False):
+        ''' Returns list of shaders of this category
         Args:
-            reload (boolean): If true, reload shaders from disk before return
+            _reload (boolean): If true, reload shaders from disk before return
         Returns:
             list: shaders objects
         '''
-        if reload:
-            self._shaders = self.collectShaders()
+        if _reload:
+            self._shaders = self.collect_shaders()
         return self._shaders
 
     def browse(self):
         ''' Open current shader folder '''
-        location = os.path.abspath(self.folder)
+        location = os.path.abspath(self.folder())
         if os.path.exists(location):
             subprocess.Popen("explorer " + location)
 
-    # ------------------------------------------------------------------------------------
-    # Collect / Generate Methods
-    # ------------------------------------------------------------------------------------
-
-    def collectShaders(self):
+    def collect_shaders(self):
         ''' Returns a list of shader objects from chosen category '''
-        folders = [x.upper() for x in os.listdir(self.folder)]
-        return [Shader.loadShader(name=f, category=self) for f in folders]
+        folders = [x.upper() for x in os.listdir(self.folder())]
+        return [Shader.load_shader(name=f, category=self) for f in folders]
 
     # ------------------------------------------------------------------------------------
     # Static Methods
@@ -74,11 +65,11 @@ class Category():
             os.mkdir(os.path.abspath(os.path.join(LIBRARY_SHADERS_PATH, name)))
 
     @staticmethod
-    def collectCategories(ui):
+    def collect_categories(ui):
         ''' Load Categories from disk and set up main storing list '''
         if not os.path.exists(LIBRARY_SHADERS_PATH):
-            informationDialog("Warning: Categories Folder not found.", ui)
-            print('Path:', LIBRARY_SHADERS_PATH)
+            information_dialog("Warning: Categories Folder not found.", ui)
+            log.warning('Path:', LIBRARY_SHADERS_PATH)
             return []
 
         folders = [x.upper() for x in os.listdir(LIBRARY_SHADERS_PATH)]
