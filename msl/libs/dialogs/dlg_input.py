81af6513bbd5d1b8828d52f25a3e45883aaa304c
Maximilliano Rocamora <maxirocamora@gmail.com>
2022-05-11 20:26:56 -0300

    snake_case

--- msl/libs/dialogs/dlg_input.py
+++ msl/libs/dialogs/dlg_input.py
@@ -1,7 +1,7 @@
 from PySide2 import QtWidgets
 
 
-def inputPathDialog(msg, ui):
+def input_path_dialog(msg, ui):
     ''' open qt dialog box when no shader is selected or found'''
     msgBox = QtWidgets.QMessageBox(ui)
     msgBox.setStyleSheet("background: rgba(40, 40, 40, 255);")
