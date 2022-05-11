72cc30b12eece994096b35b651dbddc54f796bc8
Maximilliano Rocamora <maxirocamora@gmail.com>
2022-05-11 20:27:30 -0300

    refactor, log, snake_case

--- msl/libs/shader_generator.py
+++ msl/libs/shader_generator.py
@@ -1,6 +1,7 @@
 # --------------------------------------------------------------------------------------------
 # Maya Shader Library
 # Author: maxirocamora@gmail.com
+# https://github.com/MaxRocamora/MayaShaderLibrary
 #
 # Shader Generator
 # Fills layouts with buttons widgets of shaders from the corresponding category
@@ -13,25 +14,25 @@ from contextlib import contextmanager
 
 from PySide2 import QtCore, QtGui, QtWidgets
 
-from msl import thumbnail_default_scene, QSS_BUTTON
+from msl.config import thumbnail_default_scene, QSS_BUTTON
+from msl.libs.dialogs.dlg_rename_shader import RenameShaderDialog
+from msl.libs.dialogs.dlg_delete_shader import DeleteShaderDialog
+from msl.libs.logger import log
 from msl.ui.icons import get_icon
-from msl.libs.dialogs.dlg_renameShader import renameShaderDialog
-from msl.libs.dialogs.dlg_deleteShader import deleteShaderDialog
 
-renderScript = os.path.join(
-    os.path.dirname(__file__), 'utils', 'generateThumbnail.py')
+RND_SCRIPT = os.path.join(os.path.dirname(__file__), 'utils', 'generate_thumbnail.py')
 
 MAYAPY = os.path.join(os.getenv('MAYA_LOCATION'), 'bin', 'mayapy.exe')
 
 
-def generateShaderButtons(shaderList, observer, layout, wide):
+def generate_shader_buttons(shaders, observer, layout, wide):
     '''Generate a list of button widgets from each shader object
     Args:
-        shaderList (list) List of shader objects
+        shaders (list) List of shader objects
         observer (class) Observer class to set selected shader
     '''
-    shaderButtons = []
-    for shader in shaderList:
+    shader_buttons = []
+    for shader in shaders:
         b = QtWidgets.QToolButton()
         b.setText(shader.name)
         b.setIcon(QtGui.QIcon(shader.get_thumbnail()))
@@ -42,28 +43,26 @@ def generateShaderButtons(shaderList, observer, layout, wide):
         with open(QSS_BUTTON) as fh:
             b.setStyleSheet(fh.read())
         b.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
-        b.customContextMenuRequested.connect(
-            CallMenu(shader.name, shader, observer))
+        b.customContextMenuRequested.connect(CallMenu(shader.name, shader, observer))
         b.clicked.connect(CallUser(shader, observer, b))
-        shaderButtons.append(b)
-    fillShaderLayout(layout, wide, shaderButtons)
+        shader_buttons.append(b)
+    fill_shader_layout(layout, wide, shader_buttons)
 
 
-def fillShaderLayout(layout, wide, buttonList):
-    '''
-    Fills given layout with buttons shaders
+def fill_shader_layout(layout, wide, buttons):
+    ''' Fills given layout with buttons shaders
     Args:
         layout (widget) layout widget to fill
         wide (int) maximun columns to split buttons
-        buttonList (list) list of button widgets
+        buttons (list) list of button widgets
     '''
     b = 0
     row = 0
-    while b < len(buttonList):
+    while b < len(buttons):
         for col in range(wide):
-            if b >= len(buttonList):
+            if b >= len(buttons):
                 break
-            layout.addWidget(buttonList[b], row, col)
+            layout.addWidget(buttons[b], row, col)
             b += 1
         row += 1
 
@@ -71,91 +70,87 @@ def fillShaderLayout(layout, wide, buttonList):
 class CallUser:
     ''' Menu for click on shader button '''
 
-    def __init__(self, shaderClass, observer, button):
-        self.shader = shaderClass
+    def __init__(self, _shader, observer, button):
+        self.shader = _shader
         self.observer = observer
         self.button = button
 
     def __call__(self):
-        self.observer.selectedShader = self.shader
+        self.observer.selected_shader = self.shader
 
 
 class CallMenu:
     ''' Menu for right click on shader button '''
 
-    def __init__(self, name, shaderClass, observer):
+    def __init__(self, name, _shader, observer):
         self.name = name
-        self.shader = shaderClass
+        self.shader = _shader
         self.observer = observer
 
     def __call__(self, mousePosition):
         self.observer.selectedShader = self.shader
         self.menu = QtWidgets.QMenu()
 
-        actionStr = f"Import '{self.name}'' into scene"
-        smImport = QtWidgets.QAction(get_icon("impMaya"), actionStr, self.menu)
+        text = f"Import '{self.name}'' into scene"
+        smImport = QtWidgets.QAction(get_icon("impMaya"), text, self.menu)
         self.menu.addAction(smImport)
         smImport.triggered.connect(lambda: self.shader.importShader())
         self.menu.addSeparator()
 
-        actionStr = f"Import '{self.name}'' and assign into selection"
+        text = f"Import '{self.name}'' and assign into selection"
         smImportSet = QtWidgets.QAction(
-            get_icon("impMaya"), actionStr, self.menu)
+            get_icon("impMaya"), text, self.menu)
         self.menu.addAction(smImportSet)
         smImportSet.triggered.connect(
             lambda: self.shader.importShader(assign=True))
         self.menu.addSeparator()
 
-        actionStr = f"Rename '{self.name}'"
-        smRename = QtWidgets.QAction(get_icon("rename"), actionStr, self.menu)
+        text = f"Rename '{self.name}'"
+        smRename = QtWidgets.QAction(get_icon("rename"), text, self.menu)
         self.menu.addAction(smRename)
-        smRename.triggered.connect(
-            renameShaderDialog(self.shader, self.observer))
+        smRename.triggered.connect(RenameShaderDialog(self.shader, self.observer))
         self.menu.addSeparator()
 
-        actionStr = f"Browse '{self.name}' folder on disk"
-        smBrowse = QtWidgets.QAction(get_icon("browse"), actionStr, self.menu)
+        text = f"Browse '{self.name}' folder on disk"
+        smBrowse = QtWidgets.QAction(get_icon("browse"), text, self.menu)
         self.menu.addAction(smBrowse)
         smBrowse.triggered.connect(self.shader.browse)
         self.menu.addSeparator()
 
-        actionStr = f"Generate {self.name} Thumbnail"
-        smBrowse = QtWidgets.QAction(get_icon("wips"), actionStr, self.menu)
+        text = f"Generate {self.name} Thumbnail"
+        smBrowse = QtWidgets.QAction(get_icon("wips"), text, self.menu)
         self.menu.addAction(smBrowse)
-        smBrowse.triggered.connect(lambda: self.launchThumbnail(self.shader))
+        smBrowse.triggered.connect(lambda: self.launch_thumbnail(self.shader))
         self.menu.addSeparator()
 
-        actionStr = f"Delete '{self.name}' from lib"
-        smDelete = QtWidgets.QAction(get_icon("delete"), actionStr, self.menu)
+        text = f"Delete '{self.name}' from lib"
+        smDelete = QtWidgets.QAction(get_icon("delete"), text, self.menu)
         self.menu.addAction(smDelete)
-        smDelete.triggered.connect(
-            deleteShaderDialog(self.shader, self.observer))
+        smDelete.triggered.connect(DeleteShaderDialog(self.shader, self.observer))
         self.menu.addSeparator()
         self.menu.popup(QtGui.QCursor.pos())
 
-    def launchThumbnail(self, shader):
+    def launch_thumbnail(self, shader):
         ''' Launch python script for thumbnail generation
         Render Command sends the script, the lightrig scene
         used for rendering, the shader maya file,
         and the target png file for the thumbnail.
         '''
-        # from StringIO import StringIO
-        shaderRig = os.path.abspath(thumbnail_default_scene)
+        maya_file = os.path.abspath(thumbnail_default_scene)
         if not os.path.exists(MAYAPY):
-            print('mayapy.exe not found.', MAYAPY)
+            log.error('mayapy.exe not found.', MAYAPY)
             return
 
-        cmd = ' '.join([MAYAPY, renderScript, shaderRig,
-                       shader.cgFile, shader.thumbnail]
+        cmd = ' '.join([MAYAPY, RND_SCRIPT, maya_file,
+                       shader.cg_file, shader.thumbnail]
                        )
 
         with self.waitCursor():
             process = subprocess.Popen(
                 cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             output, error = process.communicate()
-            # log_subprocess_output(StringIO(output))
-            print(output)
-            print(error)
+            log.info(output)
+            log.info(error)
 
     @contextmanager
     def waitCursor(self):
