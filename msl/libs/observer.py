57cad0eb442fbae13860af1ec4df716ebbc0c84b
Maximilliano Rocamora <maxirocamora@gmail.com>
2022-05-12 01:58:29 -0300

    properties to method

--- msl/libs/observer.py
+++ msl/libs/observer.py
@@ -3,57 +3,53 @@
 # Author: maxirocamora@gmail.com
 # https://github.com/MaxRocamora/MayaShaderLibrary
 #
-# Observer its loaded by the main app
-# Holds selected shader from UI and the main UI
+# Observer is a singleton Class holding UI, Categories, and Selected Shader
 # When a shader is selected, the shader clicked signal
-# sends itself to this class using 'selectedShader'
+# sends itself to this class using 'selected_shader'
 # property and updates ui texts
 # ----------------------------------------------------------------------------------------
 from msl.libs.logger import log
+from msl.libs.utils.singleton import Singleton
 
 
-class ObserverUI():
-    def __init__(self, main, ui):
-        self.main = main
-        self.ui = ui
-        self.shader = False
-        self.category = False
+class Observer(Singleton):
+    def __init__(self):
+        self._shader = False
+        self._category = False
         self._categories = []
+        # self.ui
+        # self.category_ctrl
 
-    @property
     def categories(self):
         return self._categories
 
-    @categories.setter
-    def categories(self, v):
+    def set_categories(self, v):
         self._categories = v
 
-    @property
-    def selected_category(self):
-        return self.category
+    def category(self):
+        ''' current selected category'''
+        return self._category
 
-    @selected_category.setter
-    def selected_category(self, v):
-        self.category = v
+    def select_category(self, v):
+        self._category = v
         if not v:
             self.shader = False
             return
         log.info(f'Category selected: {v.name()} Shaders {len(v.shaders())}')
 
-    @property
-    def selected_shader(self):
-        return self.shader
+    def shader(self):
+        ''' current selected shader '''
+        return self._shader
 
-    @selected_shader.setter
-    def selected_shader(self, v):
-        self.shader = v
+    def select_shader(self, v):
+        self._shader = v
         self.update_UI()
 
     def update_UI(self):
         ''' update ui when user selects a shader '''
         self.ui.lbl_shader_name.setText(
-            f'{self.shader.name} / {self.shader.category.name()}')
-        self.ui.lbl_shader_type.setText(self.shader.shader_type)
-        self.ui.lbl_shader_user_pc.setText(f'{self.shader.user} / {self.shader.pc}')
-        self.ui.lbl_shader_code.setText(self.shader.id_name)
-        self.ui.te_notes.setText(self.shader.notes)
+            f'{self.shader().name} / {self.shader().category.name()}')
+        self.ui.lbl_shader_type.setText(self.shader().shader_type)
+        self.ui.lbl_shader_user_pc.setText(f'{self.shader().user} / {self.shader().pc}')
+        self.ui.lbl_shader_code.setText(self.shader().id_name)
+        self.ui.te_notes.setText(self.shader().notes)
