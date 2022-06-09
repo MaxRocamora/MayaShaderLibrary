a6e8fca290d5f64265d2bde8500a56d0b62d238b
Maximilliano Rocamora <maxirocamora@gmail.com>
2022-06-09 18:40:48 -0300

    comment header

--- msl/version.py
+++ msl/version.py
@@ -1,18 +1,19 @@
-# --------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------
 # Maya Shader Library
 # Author: maxirocamora@gmail.com
 # https://github.com/MaxRocamora/MayaShaderLibrary
 #
-# --------------------------------------------------------------------------------------------
+# Version Info
+# ----------------------------------------------------------------------------------------
 '''
 01/2019 - Start Development
 03/2019 - Test Release
 04/2019 - 1.0.1 / Added version
 02/2020 - fixed import package
 11/2021 - 1.1.0 / python 3, some refactor, typos, pep
-11/2021 - 1.1.1 / docstrings, comments, some fixes
-11/2021 - 1.1.2 / autopin new category, more refactor
-01/2022 - 1.1.3 / fix typo assing = assign
+11/2021 - 1.1.1 / docstring, comments, some fixes
+11/2021 - 1.1.2 / auto-pin new category, more refactor
+01/2022 - 1.1.3 / fix typo
 05/2022 - 1.2.0 / dropped python2 support, refactor methods to snake_case
 06/2022 - 1.2.1 / fixes
 
