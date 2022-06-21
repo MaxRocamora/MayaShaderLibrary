077cac03ee6256920f05ea5eb0f0813f598a912e
Maximilliano Rocamora <maxirocamora@gmail.com>
2022-06-20 21:06:23 -0300

    shader rename and delete method moved into class

--- msl/libs/shader_widget.py
+++ msl/libs/shader_widget.py
@@ -14,10 +14,9 @@ from contextlib import contextmanager
 
 from PySide2 import QtCore, QtGui, QtWidgets
 
-from msl.libs.dialogs.dlg_rename_shader import RenameShaderDialog
-from msl.libs.dialogs.dlg_delete_shader import DeleteShaderDialog
 from msl.libs.logger import log
 from msl.libs.observer import Observer
+from msl.libs.qt_dialogs import warning_message
 from msl.ui.icons import get_icon
 from msl.config import QSS_BUTTON, thumbnail_default_scene
 
@@ -61,42 +60,86 @@ class ShaderWidget(QtWidgets.QToolButton):
         self.menu = QtWidgets.QMenu()
 
         text = f"Import '{self.name}'' into scene"
-        smImport = QtWidgets.QAction(get_icon("impMaya"), text, self.menu)
-        self.menu.addAction(smImport)
-        smImport.triggered.connect(lambda: self.shader.import_shader())
+        action_import = QtWidgets.QAction(get_icon("import"), text, self.menu)
+        self.menu.addAction(action_import)
+        action_import.triggered.connect(lambda: self.shader.import_shader())
         self.menu.addSeparator()
 
         text = f"Import '{self.name}'' and assign into selection"
-        smImportSet = QtWidgets.QAction(
-            get_icon("impMaya"), text, self.menu)
-        self.menu.addAction(smImportSet)
-        smImportSet.triggered.connect(lambda: self.shader.import_shader(assign=True))
+        action_import_sel = QtWidgets.QAction(get_icon("import"), text, self.menu)
+        self.menu.addAction(action_import_sel)
+        action_import_sel.triggered.connect(lambda: self.shader.import_shader(assign=True))
         self.menu.addSeparator()
 
         text = f"Rename '{self.name}'"
-        smRename = QtWidgets.QAction(get_icon("rename"), text, self.menu)
-        self.menu.addAction(smRename)
-        smRename.triggered.connect(RenameShaderDialog(self.shader, self.observer))
+        action_rename = QtWidgets.QAction(get_icon("rename"), text, self.menu)
+        self.menu.addAction(action_rename)
+        action_rename.triggered.connect(self.rename_shader)
         self.menu.addSeparator()
 
         text = f"Browse '{self.name}' folder on disk"
-        smBrowse = QtWidgets.QAction(get_icon("browse"), text, self.menu)
-        self.menu.addAction(smBrowse)
-        smBrowse.triggered.connect(self.shader.explore)
+        action_browse = QtWidgets.QAction(get_icon("browse"), text, self.menu)
+        self.menu.addAction(action_browse)
+        action_browse.triggered.connect(self.shader.explore)
         self.menu.addSeparator()
 
         text = f"Generate {self.name} Thumbnail"
-        smBrowse = QtWidgets.QAction(get_icon("wips"), text, self.menu)
-        self.menu.addAction(smBrowse)
-        smBrowse.triggered.connect(self.launch_thumbnail)
+        action_thumbnail = QtWidgets.QAction(get_icon("thumbnail"), text, self.menu)
+        self.menu.addAction(action_thumbnail)
+        action_thumbnail.triggered.connect(self.launch_thumbnail)
         self.menu.addSeparator()
 
         text = f"Delete '{self.name}' from lib"
-        smDelete = QtWidgets.QAction(get_icon("delete"), text, self.menu)
-        self.menu.addAction(smDelete)
-        smDelete.triggered.connect(DeleteShaderDialog(self.shader))
+        action_delete = QtWidgets.QAction(get_icon("delete"), text, self.menu)
+        self.menu.addAction(action_delete)
+        action_delete.triggered.connect(self.delete_shader)
         self.menu.addSeparator()
         self.menu.popup(QtGui.QCursor.pos())
+
+    def rename_shader(self):
+        ''' open qt dialog box for rename shader '''
+
+        new_name, result = QtWidgets.QInputDialog.getText(
+            self.observer.ui,
+            "Rename Shader",
+            'Enter Shader New Name',
+            QtWidgets.QLineEdit.Normal,
+            "default"
+        )
+
+        if not result:
+            return False
+
+        try:
+            new_name = str(new_name)
+        except (UnicodeEncodeError, UnicodeDecodeError):
+            warning_message('New name has a UnicodeEncodeError!')
+            return False
+
+        if len(new_name) <= 3:
+            warning_message('New Shader name needs at least 4 characters.')
+            return False
+
+        if any(shader.name == new_name for shader in self.shader.category.shaders()):
+            warning_message('Shader name already in use.')
+            return False
+
+        if self.shader.rename(new_name):
+            self.shader.category.reload()
+
+    def delete_shader(self):
+        ''' open qt dialog box for rename shader '''
+        msg = QtWidgets.QMessageBox(self.observer.ui)
+        msg.setWindowTitle('Confirm Deletion')
+        msg.setStandardButtons(QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.Cancel)
+        msg.setStyleSheet("background: rgba(40, 40, 40, 255);")
+        msg.setIcon(QtWidgets.QMessageBox.Warning)
+        msg.setWindowIcon(get_icon('app'))
+        msg.setText('Delete selected shader files?')
+        choice = msg.exec_()
+        if choice == QtWidgets.QMessageBox.Yes:
+            self.shader.delete()
+            self.shader.category.reload()
 
     def launch_thumbnail(self):
         ''' Launch python script for thumbnail generation
@@ -110,7 +153,7 @@ class ShaderWidget(QtWidgets.QToolButton):
             return
 
         cmd = ' '.join([MAYAPY, RND_SCRIPT, maya_file,
-                       self.shader.cg_file, self.shader.thumbnail]
+                        self.shader.cg_file, self.shader.thumbnail]
                        )
 
         with self.wait_cursor():
