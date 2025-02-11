75535fdaad0bebab96870109abf92c35fbb22729
Maximilliano Rocamora <maxirocamora@gmail.com>
2025-02-11 18:20:58 -0300

    Add compatibility for PySide6

--- msl/libs/utils/get_maya_window.py
+++ msl/libs/utils/get_maya_window.py
@@ -3,12 +3,17 @@
 # Author: maxirocamora@gmail.com
 # https://github.com/MaxRocamora/MayaShaderLibrary
 #
-# QtDesigner UI File loader for Maya PySide2 / Python 3
+# QtDesigner UI File loader for Maya PySide2/6 / Python 3
 # ----------------------------------------------------------------------------------------
 
 from maya import OpenMayaUI
-from PySide2 import QtWidgets
-from shiboken2 import wrapInstance
+
+try:
+    from PySide2 import QtWidgets
+    from shiboken2 import wrapInstance
+except ImportError:
+    from PySide6 import QtWidgets
+    from shiboken6 import wrapInstance
 
 
 def get_maya_main_window():
